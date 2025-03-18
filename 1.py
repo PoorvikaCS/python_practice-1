79f3b37c97f44dc7b6e6d812bcbff60e479f94de
PoorvikaCS <169022085+PoorvikaCS@users.noreply.github.com>
2025-03-18 07:55:06 +0530

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,17 @@
+print("hello world") #basic
+
+age=input("age: ")
+print(age)
+
+#small project
+boy_name =input("Boy Name: ")
+girl_name =input("Girl Name: ")
+print(boy_name + " loves " + girl_name )
+
+
+boy_name =input("Boy Name: ")
+boy_age = int(input("Boy's age: "))
+girl_name =input("Girl Name: ")
+girl_age = int(input("Girl's age: "))
+age_diff = boy_age - girl_age
+print(f"{boy_name} loves {girl_name} . age difference is {age_diff}")
